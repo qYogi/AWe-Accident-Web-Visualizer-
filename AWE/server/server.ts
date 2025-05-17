172cfc148636f5c01736a520bb4f80781a4510c3
qYogi <bogdanviziru18@gmail.com>
2025-05-17 21:17:03 +0300

    I've added a connection to the postress database and populated with all the 7m+ accident

--- AWE/server/server.ts
+++ AWE/server/server.ts
@@ -1,14 +1,46 @@
-const http = require("node:http");
+import * as http from "http";
+import { Pool } from "pg";
+import "dotenv/config";
 
-const hostname = "127.0.0.1";
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello, World!\n");
+const pool = new Pool({
+  host: process.env.VITE_DB_HOST,
+  port: process.env.VITE_DB_PORT,
+  user: process.env.VITE_DB_USER,
+  password: process.env.VITE_DB_PASSWORD,
+  database: process.env.VITE_DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+const server = http.createServer(async (req, res) => {
+  const { pathname, searchParams } = new URL(
+    req.url,
+    `http://${req.headers.host}`
+  );
+
+  if (pathname === "/api/accidents") {
+    const state = searchParams.get("state") || "CA";
+    const limit = parseInt(searchParams.get("limit")) || 50;
+
+    try {
+      const result = await pool.query(
+        `SELECT * FROM accidents WHERE state = $1 LIMIT $2`,
+        [state, limit]
+      );
+
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(result.rows));
+    } catch (error) {
+      res.writeHead(500);
+      res.end("Error queryiing db");
+    }
+  } else {
+    res.writeHead(404);
+    res.end("Not found");
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
