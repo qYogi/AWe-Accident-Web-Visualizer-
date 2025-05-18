b47a89b825f9bc155381469ff8645e2394d719df
qYogi <bogdanviziru18@gmail.com>
2025-05-18 13:16:13 +0300

    small refactorin regadring directories

--- AWE/client/src/App.ts
+++ AWE/client/src/App.ts
@@ -1,17 +1,19 @@
-import { Header } from "./components/header/Header";
-import { DateForm } from "./components/form/DateForm";
-import { Results } from "./components/results/Results";
+import { Header } from "./components/layout/Header";
+import { DateForm } from "./components/filters/DateForm";
+import { Results } from "./components/resultsView/Results";
 import "./styles.css";
 
 export function App(): HTMLElement {
   const app = document.createElement("div");
   app.appendChild(Header());
-  app.appendChild(DateForm({
-    onSubmit: (startDate, endDate) => {
-      console.log("Start date:", startDate);
-      console.log("End date:", endDate);
-    }
-  }));
+  app.appendChild(
+    DateForm({
+      onSubmit: (startDate, endDate) => {
+        console.log("Start date:", startDate);
+        console.log("End date:", endDate);
+      },
+    })
+  );
   app.appendChild(Results());
 
   return app;
--- AWE/client/src/components/resultsView/Results.ts
+++ AWE/client/src/components/resultsView/Results.ts
@@ -1,14 +1,14 @@
-import { Table } from "../table/Table";
+import { Table } from "./Table";
 
 export function Results(): HTMLElement {
   const results = document.createElement("div");
   results.id = "results";
-  
+
   const resultsHeader = document.createElement("h2");
   resultsHeader.textContent = "Rezultate";
   results.appendChild(resultsHeader);
-  
+
   results.appendChild(Table());
-  
+
   return results;
-} 
+}
