f054cf4c32a3f77817da17f71aecbfbb1a6d61b5
qYogi <bogdanviziru18@gmail.com>
2025-06-22 18:38:21 +0300

    did a clean up and relogic how the client side is served

--- AWE/public/js/utils/flags.js
+++
@@ -1,8 +0,0 @@
-export function extractActiveFlags(accident, flagColumns) {
-    return flagColumns
-        .filter((flag) => Object.keys(accident).some((key) => key.toLowerCase() === flag && accident[key]))
-        .map((flag) => flag
-        .split("_")
-        .map((w) => w.charAt(0).toUpperCase() + w.slice(1))
-        .join(" "));
-}
